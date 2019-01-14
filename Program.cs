87dded2065771a51daa562fefd46036d5e3b7d0a
Jozef Izso <jozef.izso@gmail.com>
2019-01-14 15:32:23 +0100

    Display information about OpenShift environment

--- Program.cs
+++ Program.cs
@@ -19,6 +19,10 @@ namespace openshift_aspnet_sample
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
                 .UseStartup<Startup>();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace openshift_aspnet_sample
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,14 +28,25 @@ namespace openshift_aspnet_sample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+
+            this.Configuration.GetSection("OPENSHIFT_BUILD");
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var config = this.Configuration;
+
+                var message = $"Hello from ASP.NET Core app.\n\n" +
+                    $"Running on OpenShift server\n\n"+
+                    $"Host: {Environment.MachineName}\n" +
+                    $"EnvironmentName: {env.EnvironmentName}\n\n" +
+                    $"OPENSHIFT_BUILD_COMMIT={config.GetValue<string>("OPENSHIFT_BUILD_COMMIT")}\n" +
+                    $"OPENSHIFT_BUILD_NAME={config.GetValue<string>("OPENSHIFT_BUILD_NAME")}\n" +
+                    $"OPENSHIFT_BUILD_NAMESPACE={config.GetValue<string>("OPENSHIFT_BUILD_NAMESPACE")}\n" +
+                    $"OPENSHIFT_BUILD_REFERENCE={config.GetValue<string>("OPENSHIFT_BUILD_REFERENCE")}\n" +
+                    $"OPENSHIFT_BUILD_SOURCE={config.GetValue<string>("OPENSHIFT_BUILD_SOURCE")}\n"; ;
+
+                await context.Response.WriteAsync(message);
             });
         }
     }
